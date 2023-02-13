5863066e89eb5f325078130c439bfdc664f8164f
Łukasz Sawina <lukisaw01@gmail.com>
2023-02-13 20:15:59 +0100

    MVC implementation:

---
+++ StockExchangeSystem_Server/StockExchangeSystem_Server/WebApp/Controllers/CryptoController.cs
@@ -0,0 +1,22 @@
+﻿using CurrencyExchangeLibrary.Data;
+using CurrencyExchangeLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CryptoController : Controller
+    {
+        private readonly ICryptoRepository _cryptoRepository;
+
+        public CryptoController(ICryptoRepository cryptoRepository)
+        {
+            _cryptoRepository = cryptoRepository;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var crypto = await _cryptoRepository.GetCryptoAsync("BTC");
+            return View(crypto);
+        }
+    }
+}
---
+++ StockExchangeSystem_Server/StockExchangeSystem_Server/WebApp/Program.cs
@@ -0,0 +1,65 @@
+using CurrencyExchangeLibrary.Data;
+using CurrencyExchangeLibrary.Interfaces;
+using CurrencyExchangeLibrary.Repository;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICryptoRepository, CryptoRepository>();
+builder.Services.AddScoped<IAPIKeyLogic, APIKeyLogic>();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+try
+{
+    Log.Information("App is running");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "The application failed to start correctyly :(");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
