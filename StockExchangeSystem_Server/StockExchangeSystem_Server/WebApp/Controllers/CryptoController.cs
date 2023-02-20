a9a02be6159552da4567135d21bacbd3160e71c5
Łukasz Sawina <lukisaw01@gmail.com>
2023-02-20 18:45:23 +0100

    Crypto Page implement

--- StockExchangeSystem_Server/StockExchangeSystem_Server/WebApp/Controllers/CryptoController.cs
+++ StockExchangeSystem_Server/StockExchangeSystem_Server/WebApp/Controllers/CryptoController.cs
@@ -2,6 +2,7 @@
 using CurrencyExchangeLibrary.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -18,6 +19,21 @@ namespace WebApp.Controllers
         {
             var crypto = await _cryptoRepository.GetCryptosAsync();
             return View(crypto);
+        }
+
+        public async Task<IActionResult> CryptoPage(string symbol)
+        {
+            var crypto = await _cryptoRepository.GetCryptoAsync(symbol);
+            var OHLCV = await _cryptoRepository.GetCryptoOHLCVAsync(symbol);
+
+            var output = new CryptoViewModel()
+            { 
+                Crypto = crypto,
+                OHLCV = OHLCV
+            };
+
+
+            return View(output);
         }
 
     }
---
+++ StockExchangeSystem_Server/StockExchangeSystem_Server/WebApp/Models/CryptoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CurrencyExchangeLibrary.Models.Crypto;
+using CurrencyExchangeLibrary.Models.OHLC;
+
+namespace WebApp.Models
+{
+    public class CryptoViewModel
+    {
+        public CryptoModel Crypto { get; set; }
+        public List<OHLCVCryptoModel> OHLCV { get; set; }
+    }
+}
